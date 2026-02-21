6245be44a14c8a923aa96a2d5024ee4c37785eaa
vishwadeep sankpal <vishwadeep108@gmail.com>
2026-02-21 14:55:00 +0530

    migration hit

---
+++ DevLog.Api/DevLog.Api/Migrations/20260221082450_thumbnail_update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevLog.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class thumbnail_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Thumbnail",
+                table: "Posts",
+                newName: "ThumbnailUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailPublicId",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailPublicId",
+                table: "Posts");
+
+            migrationBuilder.RenameColumn(
+                name: "ThumbnailUrl",
+                table: "Posts",
+                newName: "Thumbnail");
+        }
+    }
+}
