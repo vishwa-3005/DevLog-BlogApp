bbd5ce2e24f826e710a62186b7d6756e45578f49
vishwadeep sankpal <vishwadeep108@gmail.com>
2026-02-21 16:49:11 +0530

    Project setup, routing and Redux store initialized

---
+++ DevLog.Client/Devlog.Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
